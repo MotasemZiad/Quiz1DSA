dc51c37c6e643c5ccc31384e95f7090e6fa92662
MotasemZiad <moatasem.abunema@gmail.com>
2022-10-22 14:52:22 +0300

    some updates

--- Quiz1.java
+++ Quiz1.java
@@ -6,11 +6,13 @@ public class Quiz1 {
 
     public static void main(String[] args) {
         Circle c1 = new Circle(29.4);
-
-        System.out.println(c1.getArea());
         Rectangle r1 = new Rectangle(new Rectangle.Dimension(3, 6));
 
+        System.out.println(c1.getArea());
         System.out.println(r1.getArea());
+
+        System.out.println(c1.toString());
+        System.out.println(r1.toString());
     }
 
 }
